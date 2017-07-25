20b57d3c0b5bca1c7ee6fed25876b305f0fa0b8d
vincent3569 <vincent_bourganel@yahoo.fr>
2017-07-25 16:35:16 +0200

    Update themeoptions.php
    typo

--- themeoptions.php
+++ themeoptions.php
@@ -38,7 +38,7 @@ class ThemeOptions {
 		setThemeOptionDefault('use_colorbox_image', false);
 		setThemeOptionDefault('show_exif', true);
 
-		enableExtension('colorbox_js', 800 | THEME_PLUGIN;);
+		enableExtension('colorbox_js', 5|THEME_PLUGIN);
 		setOption('colorbox_zpArdoise_album', 1);
 		setOption('colorbox_zpArdoise_archive', 1);
 		setOption('colorbox_zpArdoise_contact', 1);
