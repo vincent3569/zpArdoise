692e020cb5e40754dccf6d9317cb77b9dbf4b8c0
vincent3569 <vincent_bourganel@yahoo.fr>
2017-10-14 12:02:08 +0200

    official design of Zenphoto

--- theme_description.php
+++ theme_description.php
@@ -4,5 +4,5 @@ $theme_description['name'] = 'zpArdoise';
 $theme_description['author'] = 'Vincent3569';
 $theme_description['version'] = '1.4.14';
 $theme_description['date'] = '27/09/2017';
-$theme_description['desc'] = 'ZenPhoto/ZenPage theme, inherited from initial release of zpGalleriffic theme. Please read <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/zpArdoise/readme.htm' . '" title="Readme File" target="_blank">Readme</a>, <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/zpArdoise/license.htm' . '" title="License File" target="_blank">License</a> and <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/zpArdoise/changelog.txt' . '" title="ChangeLog File" target="_blank">ChangeLog</a>';
+$theme_description['desc'] = 'Zenphoto theme, inherited from initial release of zpGalleriffic theme. Please read <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/zpArdoise/readme.htm' . '" title="Readme File" target="_blank">Readme</a>, <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/zpArdoise/license.htm' . '" title="License File" target="_blank">License</a> and <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/zpArdoise/changelog.txt' . '" title="ChangeLog File" target="_blank">ChangeLog</a>';
 ?>
